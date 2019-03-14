43473886faebbf5d0102c867155d3fc8c26ec57a
Zeljko <zmalic@gmail.com>
2019-03-14 17:46:13 +0100

    Project with a simply example scene

---
+++ Assets/SLoader/Example/Scripts/SimpleCameraOrbit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCameraOrbit : MonoBehaviour
+{
+    public float horisontalSpeed = 2.5f;
+    public float verticalSpeed = 2.5f;
+    
+    float x = 0.0f;
+    float y = 0.0f;
+
+    void LateUpdate()
+    {
+        x += Input.GetAxis("Mouse X") * horisontalSpeed;
+        y -= Input.GetAxis("Mouse Y") * verticalSpeed;
+        
+        transform.rotation = Quaternion.Euler(y, x, 0);
+    }
+}
